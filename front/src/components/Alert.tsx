7d97f71eef16b31f8edeb9d69b1e1351103ee3c8
Soroush Alinia <soroushalinia.wm@gmail.com>
2024-07-10 19:06:43 +0330

    Remove Frontend

--- front/src/components/Alert.tsx
+++
@@ -1,9 +0,0 @@
-import { AlertProps } from "../types";
-
-export default function Alert(props: AlertProps) {
-  return (
-    <div className={`flex flex-row justify-center ${props.type === "info" ? "bg-green-100 border border-green-400" : "bg-red-100 border border-red-400"} p-6 rounded-lg `}>
-      <p className={`${props.type === "info" ? "text-green-400" : "text-red-400"}`}>{props.msg}</p>
-    </div>
-  );
-}
